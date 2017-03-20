8e9368882578fa15da1f52584c7430cef110a75c
eygz <xormrr@gmail.com>
2017-03-20 22:40:45 +0300

    fixed random strategy

--- src/main/strategy/RandomStrategy.java
+++ src/main/strategy/RandomStrategy.java
@@ -1,26 +1,34 @@
 package main.strategy;
 
+import java.util.Random;
 import java.util.Set;
 
 import main.Utils;
 import main.common.Move;
 import main.common.Turn;
-import main.utils.Randomizer;
 
 /**
  * Does random moves.
  */
 public class RandomStrategy implements Strategy {
 
-	private final Randomizer<Move> randomizer = Randomizer.create(0);
+	private final Random random = new Random(0);
 	
 	@Override
 	public Move move(Turn turn) {
 		Set<Move> possibleMoves = Utils.getPossibleMoves(turn);
 		assert(!possibleMoves.isEmpty()) : "No possible moves?";
 		
-		randomizer.addAll(possibleMoves);
-		return randomizer.remove();
+		// Random element index
+		int k = random.nextInt(possibleMoves.size());
+		
+		// Iterate to the k-th element
+		for (Move move : possibleMoves)
+			if (k-- == 0)
+				return move;
+
+		// unreachable
+		throw new IllegalStateException();
 	}
 
 }
