aa0109437ab2e1c78f1c679303b63a3b98b6e2ea
Rodion Mulyukov <eygz@ispras.ru>
2017-03-21 14:31:14 +0300

    Colorful console!

--- src/main/strategy/ConsoleStrategy.java
+++ src/main/strategy/ConsoleStrategy.java
@@ -20,7 +20,10 @@ public class ConsoleStrategy implements Strategy {
 
 	private static final Scanner in = new Scanner(System.in);
 	private static final PrintStream out = System.out;
+	private static final PrintStream err = System.err;
 
+	private static final int consoleFlushIntervals = 10; // Sleep time (ms)
+	
 	@Override
 	public Move move(Turn turn) {
 		printTurn(turn);
@@ -29,41 +32,51 @@ public class ConsoleStrategy implements Strategy {
 	}
 
 	private void printTurn(Turn turn) {
-		out.println("============================================");
+		println("============================================");
 
 		printDeadCards(turn);
 		CommonInfo commonInfo = turn.getCommonInfo();
 
 		// Turn type
-		out.println(commonInfo.getTurnType() + ", Deck: " + commonInfo.getRemainingDeckCardsNumber() + ", Trump: "
-				+ turn.getCommonInfo().getDeckBottomCard() + ", Enemy: " + turn.getOpponentCardsNumber());
+		print(commonInfo.getTurnType() + ", Deck: " + commonInfo.getRemainingDeckCardsNumber() + ", Trump: ");
+		printCard(turn.getCommonInfo().getDeckBottomCard());
+		println(", Enemy: " + turn.getOpponentCardsNumber());
 
 		printMyHand(turn);
 		// Card fights
-		if (commonInfo.getFights() != null) {
-			out.print("Def: ");
-			for (CardFight cardFight : commonInfo.getFights())
-				out.print(cardFight.getDefender() + " ");
-			out.println();
-			out.print("Att: ");
-			for (CardFight cardFight : commonInfo.getFights())
-				out.print(cardFight.getAttacker() + " ");
-			out.println();
+		if (!commonInfo.getFights().isEmpty()) {
+			print("Def: ");
+			for (CardFight cardFight : commonInfo.getFights()) {
+				printCard(cardFight.getDefender());
+				print(" ");
+			}
+			println();
+			print("Att: ");
+			for (CardFight cardFight : commonInfo.getFights()) {
+				printCard(cardFight.getAttacker());
+				print(" ");
+			}
+			println();
 		}
 
 		// Attacker card
-		if (commonInfo.getTurnType().equals(TurnType.Defence))
-			out.println("Attacker: " + commonInfo.getAttacker());
+		if (commonInfo.getTurnType().equals(TurnType.Defence)) {
+			print("Attacker: ");
+			printCard(commonInfo.getAttacker());
+			println();
+		}
 
 		// Possible moves
-		out.print("Moves: ");
+		print("Moves: ");
 
 		Set<Move> possibleMoves = Utils.getPossibleMoves(turn);
 		Move[] possibleMovesSorted = Utils.sortMoves(possibleMoves);
-		for (Move move : possibleMovesSorted)
-			out.print(move + " ");
+		for (Move move : possibleMovesSorted) {
+			printMove(move);
+			print(" ");
+		}
 
-		out.println();
+		println();
 	}
 
 	private Move readMove(Turn turn) {
@@ -72,13 +85,13 @@ public class ConsoleStrategy implements Strategy {
 
 			Move move = parseInput(input);
 			if (move == null) {
-				out.print("Couldn't parse the move. Try again: ");
+				print("Couldn't parse the move. Try again: ");
 				continue;
 			}
 
 			Set<Move> possibleMoves = Utils.getPossibleMoves(turn);
 			if (!possibleMoves.contains(move)) {
-				out.print("You can't proceed with such move. Try again: ");
+				print("You can't proceed with such move. Try again: ");
 				continue;
 			}
 
@@ -100,19 +113,81 @@ public class ConsoleStrategy implements Strategy {
 
 	private void printCardSet(Set<Card> cardSet, String name) {
 		Card[] cardSetSorted = Utils.sortCards(cardSet);
-		out.print(name + ": ");
-		for (Card card : cardSetSorted)
-			out.print(card + " ");
+		print(name + ": ");
+		for (Card card : cardSetSorted) {
+			printCard(card);
+			print(" ");
+		}
+		println();
+	}
+	
+	private void printMove(Move move) {
+		switch (move.getMoveType()) {
+		case Finish:
+			print("fin");
+			break;
+		case UseCard:
+			printCard(move.getCard());
+			break;
+		}
+		out.flush();
+	}
+	
+	private void printCard(Card card) {
+		if (card == null)
+			return;
+		
+		switch (card.getSuit()) {
+		case Clubs:
+		case Spades:
+			print(card);
+			break;
+		case Diamonds:
+		case Hearts:
+			printRed(card);
+			break;
+		}
+	}
+	
+	private static void printRed(Object obj) {
+		err.print(obj.toString());
+		err.flush();
+		sleep();
+	}
+	
+	private static void print(Object obj) {
+		out.print(obj.toString());
+		out.flush();
+		sleep();
+	}
+	
+	private static void println(Object obj) {
+		out.println(obj.toString());
+		out.flush();
+		sleep();
+	}
+	
+	private static void println() {
 		out.println();
+		out.flush();
+		sleep();
 	}
 
+	private static void sleep() {
+		try {
+			Thread.sleep(consoleFlushIntervals);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	/**
 	 * @return null when can't parse
 	 */
 	private Move parseInput(String input) {
 		input = input.toUpperCase();
 
-		if (input.equals("FINISH") || input.equals("F"))
+		if (input.equals("FIN") || input.equals("F"))
 			return Move.createFinishMove();
 
 		input = input.replace('H', '♥').replace('C', '♣').replace('D', '♦').replace('S', '♠');
